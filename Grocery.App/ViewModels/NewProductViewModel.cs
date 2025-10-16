081393d9636c282de142e4db43fe617b99adb960
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2025-10-16 13:46:29 +0200

    base commit for UC19
    + database calls in the Repo
    + views and models and routes

---
+++ Grocery.App/ViewModels/NewProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Grocery.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.App.ViewModels
+{
+    public abstract class NewProductViewModel : BaseViewModel
+    {
+        private readonly IProductService _productService;
+
+        public NewProductViewModel(IProductService productService)
+        {
+            _productService = productService;
+        }
+    }
+}
---
+++ Grocery.App/Views/NewProductView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Grocery.App.Views;
+
+public partial class NewProductView : ContentView
+{
+	public NewProductView()
+	{
+		InitializeComponent();
+	}
+}
